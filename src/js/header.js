75270997658639ea0edf121192d774763bf0c40c
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-10-12 23:43:16 +0300

    header fix

--- src/js/header.js
+++ src/js/header.js
@@ -8,11 +8,13 @@ const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
 const DEBOUNCE_DELAY = 500;
 const refs = {
   input: document.querySelector('.header-input'),
+  mobileInput: document.querySelector('header-input-mobile'),
   btn: document.querySelector('.menu-batton'),
   mobileMenu: document.querySelector('.header-mobile-menu'),
 };
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+// refs.mobileInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(evt) {
   let serchQuery = evt.target.value;
   fetch(`${BASE_URL}search.php?s=${serchQuery}`)
@@ -22,28 +24,14 @@ function onInput(evt) {
       console.log(arrayLength);
       mainFunction(1, `${BASE_URL}search.php?s=${serchQuery}`, arrayLength);
     });
-
-  // getCocktails(serchQuery)
-  //   .then(data => console.log(data))
-  //   .catch(erro => {
-  //     console.log(erro);
-  //   });
 }
-
-// function getCocktails(query) {
-//   return fetch(`${BASE_URL}search.php?s=${query}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   });
-// }
 // ______________________________________________________
 // menu btn
 refs.btn.addEventListener('click', onClick);
 function onClick() {
   const expanded = refs.btn.getAttribute('aria-expanded') === 'true' || false;
 
+  refs.btn.classList.toggle('is-open');
   refs.mobileMenu.classList.toggle('is-open');
   refs.btn.setAttribute('aria-expanded', !expanded);
 }
